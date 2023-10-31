48e7d85a1cff53fdafcbdf0035218e4b661b69ae
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-10-31 17:05:43 -0500

    Configure Wepback to be in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'production',
+module.exports = {  
+  // Development mode makes it easy to debug code, but code is not as efficient as it could be.
+  // Production mode builds the code to be as efficient as possible, but is hard to debug.
+
+  // To switch to production: 
+    // change mode to production
+    // and remove line with devtool
+  // Now just rebuild the project to get production code.
+
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),

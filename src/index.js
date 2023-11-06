359abe2a405b0aa5ef32d152fd8ea499c1bbe671
haydenkolb <145166815+haydenkolb@users.noreply.github.com>
2023-11-05 18:34:08 -0600

    Revert "Game mode selection added"

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Sign in form submitTypeInput holds value of submitted method
 const submitTypeInput = document.getElementById('submit-type');
+const gameModeTypeInput = document.getElementById('mode-type');
 
 // Set submitTypeInput's value when sign-in-button is clicked
 document.getElementById('sign-in-button').addEventListener('click', () => {
@@ -30,143 +30,36 @@ document.getElementById('register-button').addEventListener('click', () => {
   submitTypeInput.value = 'register';
 });
 
-// Declaration of Street view service, map, and panorama variables.
-let sv;
-let map;
-let panorama;
-
-// Callback function for Google maps API
-async function initialize() {
-  sv = new google.maps.StreetViewService();
-  panorama = new google.maps.StreetViewPanorama(
-    document.getElementById('pano'),
-  );
-
-  // Disable the road names that are displayed in the panoramic street view by default
-  panorama.setOptions({
-    showRoadLabels: false,
-    addressControl: false,
-  });
-
-  // Set up the guessing map
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 0, lng: 0 },
-    zoom: 1,
-    streetViewControl: false,
-    disableDefaultUI: true,
-  });
-}
-window.initialize = initialize;
-
-// Generates a random set of coordinates
-async function getRandomLocation() {
-  const minLat = -90; // Minimum latitude
-  const maxLat = 90; // Maximum latitude
-  const minLng = -180; // Minimum longitude
-  const maxLng = 180; // Maximum longitude
-  const randomLat = Math.random() * (maxLat - minLat) + minLat;
-  const randomLng = Math.random() * (maxLng - minLng) + minLng;
-  const location = await new google.maps.LatLng(randomLat, randomLng);
-  return location;
-}
-
-function processSVData(loc, rad) {
-  sv.getPanorama({ location: loc, radius: rad }, (data, status) => {
-    console.log(status);
-    if (status === google.maps.StreetViewStatus.ZERO_RESULTS) {
-      processSVData(loc, rad * 10);
-    } else if (status === google.maps.StreetViewStatus.OK) {
-      panorama.setPano(data.location.pano);
-      panorama.setPov({
-        heading: 270,
-        pitch: 0,
-      });
-      panorama.setVisible(true);
-    } else {
-      console.alert('Error getting panoramic street view');
-    }
-  });
-}
-
-async function playSoloGame(roundDuration, round) {
-  // TODO
-  // Run the game with specified roundDuration & increment round after each round ends
-
-  // Generate a random location for this round
-  const location = await getRandomLocation();
-  console.log('Location' + location);
-
-  const map = document.getElementById('map');
-  map.style.display = 'block';
-  const panorama = document.getElementById('pano');
-  panorama.style.display = 'block';
-
-  processSVData(location, 100);
-}
-// Game mode selection form gameModeTypeInput holds value of submitted game type
-const gameModeTypeInput = document.getElementById('mode-type');
-
-// Get user selected game mode
+// Hide auth container and display the game mode selection form and add event listeners
 function getGamemode() {
-  // Hide auth container and display the game mode selection form and add event listeners
   const signInForm = document.getElementById('sign-in-form');
   signInForm.style.display = 'none';
   const gameModeForm = document.getElementById('game-mode-form');
   gameModeForm.style.display = 'block';
 
-  // Event listeners for game mode selection buttons
   document.getElementById('normal-diff').addEventListener('click', () => {
     gameModeTypeInput.value = 'normal-solo';
   });
+
   document.getElementById('hard-diff').addEventListener('click', () => {
     gameModeTypeInput.value = 'hard-solo';
   });
+
   document.getElementById('expert-diff').addEventListener('click', () => {
     gameModeTypeInput.value = 'expert-solo';
   });
+
   document.getElementById('battle-royale').addEventListener('click', () => {
     gameModeTypeInput.value = 'battle-royale';
   });
+
   document.getElementById('hide-and-seek').addEventListener('click', () => {
     gameModeTypeInput.value = 'hide-and-seek';
   });
 
-  // Submit event listener for gamemode selection form
   gameModeForm.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(gameModeTypeInput.value);
-    const gameMode = gameModeTypeInput.value;
-    let roundDuration;
-
-    // Hide the game mode form
-    gameModeForm.style.display = 'none';
-
-    // Determine round duration for the solo game mode & handle case for multiplayer modes selection
-    switch (gameMode) {
-      case 'normal-solo':
-        roundDuration = 120;
-        playSoloGame(roundDuration, 1);
-        break;
-      case 'hard-solo':
-        roundDuration = 60;
-        playSoloGame(roundDuration, 1);
-        break;
-      case 'expert-solo':
-        roundDuration = 30;
-        playSoloGame(roundDuration, 1);
-        break;
-      case 'battle-royale':
-        // TODO
-        // Display the lobby selection page & host lobby button
-        break;
-      case 'hide-and-seek':
-        // TODO
-        // Display the lobby selection page & host lobby button
-        break;
-      default:
-        console.log('default case');
-        break;
-    }
   });
 }
 

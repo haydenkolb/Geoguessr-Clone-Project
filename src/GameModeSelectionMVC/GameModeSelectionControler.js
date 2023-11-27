a99e8b1061b69174b5c69d0b777f2cd2eed7bcf1
Rosendo Martinez <rosendomartinez270@gmail.com>
2023-11-26 18:07:50 -0600

    Add max solo rounds constant

--- src/GameModeSelectionMVC/GameModeSelectionControler.js
+++ src/GameModeSelectionMVC/GameModeSelectionControler.js
@@ -1,7 +1,7 @@
 class GameModeSelectionControler {
-    constructor(gameModeSelectionView, soloGameCallback) {
+    constructor(gameModeSelectionView, soloGameMVCCallback) {
         this.gameModeSelectionView = gameModeSelectionView;
-        this.soloGameCallback = soloGameCallback;
+        this.soloGameMVCCallback = soloGameMVCCallback;
         this.roundDuration = {
             'normal-solo': 120,
             'hard-solo': 60,
@@ -15,16 +15,19 @@ class GameModeSelectionControler {
     handleGameModeSelection = (gameMode) => {
         this.gameModeSelectionView.hideGameModeForm();
 
+        // TODO: figure out better place for this const
+        const MAX_ROUNDS_PER_SOLO_GAME = 5;
+
         // Determine round duration for the solo game mode & handle case for multiplayer modes selection
         switch (gameMode) {
             case 'normal-solo':
-                this.soloGameCallback(this.roundDuration['normal-solo'],1,0)
+                this.soloGameMVCCallback(MAX_ROUNDS_PER_SOLO_GAME, this.roundDuration['normal-solo'])
                 break;
             case 'hard-solo':
-                this.soloGameCallback(this.roundDuration['hard-solo'],1,0)
+                this.soloGameMVCCallback(MAX_ROUNDS_PER_SOLO_GAME, this.roundDuration['hard-solo'])
                 break;
             case 'expert-solo':
-                this.soloGameCallback(this.roundDuration['expert-solo'],1,0)
+                this.soloGameMVCCallback(MAX_ROUNDS_PER_SOLO_GAME, this.roundDuration['expert-solo'])
                 break;
             case 'battle-royale':
                 // TODO: Display the lobby selection page & host lobby button
